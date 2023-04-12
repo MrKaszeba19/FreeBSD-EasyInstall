9f44685079a2fe9b018a7cba8f671d402d9e3522
RooiGevaar19 <redthreat19@gmail.com>
2023-04-12 12:14:37 +0200

    Add all users dialog

--- FreeBSD-EasyInstall.sh
+++ FreeBSD-EasyInstall.sh
@@ -81,15 +81,39 @@ setup_user () {
 }
 
 configure_users_dialog () {
-    usern="user"
-    while [ "s$usern" != "s" ]
-    do
+    dktop_name=$1
+    
+    ans_a="1"
+    flag="0"
+    while [ "$flag" != "1" ]
         echo
-        echo "Type an username of the user (or press RETURN to quit)"
-        read usern
-        if [ "s$usern" != "s" ] ;
+        echo "What do you want to do?"
+        echo "    1 - Make all users be able to log in via $dktop_name"
+        echo "    2 - Choose which users can log in via $dktop_name"
+        read ans_a
+        echo $ans_a | grep "[^(1|2)]" > /dev/null 2>&1
+        if [ "$?" -ne "0" ]; 
         then
-            setup_user $usern
+            flag="1"
+            if [ "$ans_a" = "1" ] ;
+            then
+                for item in `cat /etc/passwd | awk -F':' '{if ($3 >= 1000 && $3 <= 10000) print $1;}'`
+                do
+                    setup_user $item
+                done
+            else
+                usern="user"
+                while [ "s$usern" != "s" ]
+                do 
+                    echo
+                    echo "Type an username of the user (or press RETURN to quit)"
+                    read usern
+                    if [ "s$usern" != "s" ] ;
+                    then
+                        setup_user $usern
+                    fi
+                done
+            fi
         fi
     done
 
@@ -104,25 +128,30 @@ configure_users_dialog () {
 #
 # -----------------------------------------------
 # install necessary things
+
+dktop="xfce"
+dktop_name="XFCE"
+
 echo "------------------------------------------------------------------"
 echo "Updating FreeBSD packages..."
 update_packages
 
 echo "------------------------------------------------------------------"
-echo "Installing XFCE..."
-install_desktop xfce
+echo "Installing $dktop_name..."
+install_desktop $dktop
 
 #------------------------------------------------
 # set up xfce things
 echo "------------------------------------------------------------------"
-echo "Applying global XFCE settings..."
-configure_desktop xfce
+echo "Applying global $dktop_name settings..."
+configure_desktop $dktop
 
 #------------------------------------------------
 # set up users that might log in using XFCE
+echo
 echo "------------------------------------------------------------------"
-echo "Setting up users that may log in via XFCE"
-configure_users_dialog
+echo "Setting up users that may log in via $dktop_name..."
+configure_users_dialog $dktop_name
 
 echo
 echo "------------------------------------------------------------------"
